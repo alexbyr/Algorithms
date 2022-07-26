3b11bcc932742e89e989718503b3d3f483ffb7ab
alby <byrkun@gmail.com>
2022-07-26 14:17:17 +0200

    comit

--- LinkedList.cs
+++ LinkedList.cs
@@ -1,10 +1,100 @@
 ﻿using System;
+using System.Text;
+
 namespace Algorytms
 {
     public class LinkedList
     {
-        public LinkedList()
+        public LinkedList? Next;
+        public int Data { get; set; }
+
+        
+        public LinkedList(int data=0)
         {
+            Data = data;
+        }
+
+        public void appendToTail(int d)
+        {
+            LinkedList end = new LinkedList(d);
+            LinkedList n = this;
+            while(n.Next != null)
+            {
+                n = n.Next;
+            }
+            n.Next = end;
+        }
+
+        public string Print()
+        {
+            StringBuilder sb = new StringBuilder();
+            LinkedList? n = this;
+            while (n != null)
+            {
+                Console.Write(n.Data);
+                sb.Append($"{n.Data} ");
+                n = n.Next;
+            }
+            Console.WriteLine();
+            return sb.ToString();
+        }
+
+        public static void RemoveDuplicates(LinkedList head)
+        {
+            LinkedList? current = head;
+            while(current !=null)
+            {
+                LinkedList runner = current;
+                while(runner.Next != null)
+                {
+                    if(runner.Next.Data == current.Data)
+                    {
+                        runner.Next = runner.Next.Next;
+                    }
+                    else
+                        runner = runner.Next;
+                }
+                current = current.Next;
+            }
+        }
+    }
+
+    public static class LinkedListAlgs
+    {
+        public static void Task_2_4()
+        {
+            LinkedList head = new LinkedList(3);
+            head.appendToTail(5);
+            head.appendToTail(8);
+            head.appendToTail(5);
+            head.appendToTail(9);
+            head.appendToTail(2);
+            head.appendToTail(1);
+
+            head.Print();
+        }
+
+        public static void Task_2_1()
+        {
+            LinkedList head = new LinkedList(1);
+            head.appendToTail(4);
+            head.appendToTail(8);
+            head.appendToTail(1);
+            head.appendToTail(8);
+            head.appendToTail(7);
+            head.appendToTail(8);
+            head.appendToTail(9);
+            head.appendToTail(8);
+            head.appendToTail(8);
+            head.appendToTail(7);
+            head.appendToTail(7);
+            head.appendToTail(7);
+            head.appendToTail(7);
+
+            head.Print();
+            LinkedList.RemoveDuplicates(head);
+            head.Print();
+
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,21 +2,4 @@
 using Algorytms;
 
 
-int[,] image = new int[,]
-{
-    {11, 12, 13, 14, 15, 16},
-    {21, 22, 23, 24, 25, 26},
-    {31, 32, 33, 34, 35, 36},
-    {41, 42, 43, 44, 45, 46},
-    {51, 52, 53, 54, 55, 56},
-    {61, 62, 63, 64, 65, 66}
-};
-
-
-ArraysAndStrings.Task_1_7_PrintArray(image);
-Console.WriteLine("------------------");
-ArraysAndStrings.task_1_7(image);
-ArraysAndStrings.Task_1_7_PrintArray(image);
-
-//Console.WriteLine();
-
+LinkedListAlgs.Task_2_4();
